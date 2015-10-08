correct total log likelihood to avoid overflow

mcmc_general/mcmc.py
@@ -33,8 +33,8 @@ def jump(para_old, para_stepsize, single_jump):
 	n_para = len(para_old)
 	if single_jump:
 		ind_para = np.random.choice(n_para,1)
-		para_new = para_old
-		para_new[ind_para] += para_stepsize[ind_para] * np.random.normal(0.,1.)
+		para_new = para_old + 0.
+		para_new[ind_para] = para_new[ind_para] + para_stepsize[ind_para] * np.random.normal(0.,1.)
 	else:
 		para_new = para_old + para_stepsize * np.random.normal(0.,1.,n_para)
 	return para_new
@@ -62,7 +62,7 @@ def mcmc(p0, p_step, n_step, log_likely_func, data=[], seed=2357, *check_func):
 	# advance with p_step and check if accept p_new
 	for i_step in range(1,n_step):
 
-		p_old = p_seq[:, i_step-1]
+		p_old = p_seq[:, i_step-1]+0.
 		p_new = p_old + p_step * np.random.uniform(-1,1,n_para)
 
 		delta_log = log_likely_func(p_new, *data) - \
@@ -98,6 +98,7 @@ def hbm_initial(hyper0, n_step):
 
 ''' hbm p(data|hyper)'''
 # sum of log average likelihood
+# DONT log{mean[exp(loglikelihood)]}, causing overflow and making comparison impossible
 def hbm_likelihood(hyper,draw_local_func,draw_times,n_group,log_likely_func,model,data=[]):
 	n_local = len(draw_local_func(hyper))
 	loglike_each = np.zeros((draw_times, n_group))
@@ -105,11 +106,15 @@ def hbm_likelihood(hyper,draw_local_func,draw_times,n_group,log_likely_func,mode
 		for i_draw in range(draw_times):
 			local = draw_local_func(hyper)
 			loglike_each[i_draw,i_group] = log_likely_func(local, model, *data)
-	like_each = np.exp(loglike_each)
-	like_group = np.mean(like_each,axis=0)
-	loglike = np.sum(np.log(like_group))
 
-	return loglike
+	median = np.median(loglike_each,axis=0)
+	delta = loglike_each-median
+	mean_ratio_of_median = np.mean(np.exp(delta),axis=0)
+	
+	loglikelihood = median + np.log(mean_ratio_of_median)
+	total_loglike = np.sum(loglikelihood)
+
+	return total_loglike
 
 
 ''' hbm with mcmc '''
@@ -119,36 +124,47 @@ data=[], seed=2357, domain=[], draw_times=1, single_jump = False, trial_upbound 
 	np.random.seed(seed)
 
 	### initial setup: hyper, local, loglike
-	hyper, loglike, repeat = hbm_initial(hyper0, n_step)
-	loglike[0] = hbm_likelihood(hyper0,draw_local_func,draw_times,n_group,log_likely_func,model,data)
+	hyper_chain, loglike, repeat = hbm_initial(hyper0, n_step)
+	loglike0 = hbm_likelihood(hyper0,draw_local_func,draw_times,n_group,log_likely_func,model,data)
+	loglike[0] = loglike0	
+
+	loglike_old = loglike0
+	hyper_old = hyper0
+
 
 	### run mcmc: hyper walks, generates local, calculate delta_log, accept/reject
 	for i_step in range(0, n_step-1):
-		step_stay = True		
+				
+		if (np.sum(repeat)>=trial_upbound):
+			break
+
+		step_stay = True
 		while step_stay & (np.sum(repeat)<trial_upbound):
 			repeat[i_step] = repeat[i_step]+1
 
-			hyper_old = hyper[:, i_step]
 			hyper_new = jump(hyper_old, hyper_stepsize, single_jump)
 
 			# reject new step if out of domain
 			if len(domain) != 0:
-				if not domain_pass(hyper_new,domain): continue
+				if not domain_pass(hyper_new,domain): 
+					continue
 			
 			loglike_new = hbm_likelihood(hyper_new,draw_local_func,draw_times,n_group,log_likely_func,model,data)
-
-			ratio = np.exp(loglike_new - loglike[i_step])
-
+			
+			ratio_tmp = np.exp(loglike_new - loglike_old)
+	
 			# accept new step
-			if (np.random.uniform(0,1) < ratio):
-				hyper[:,i_step+1] = hyper_new
+			if (np.random.uniform(0,1) < ratio_tmp):
+				hyper_chain[:,i_step+1] = hyper_new
+				hyper_old = hyper_new + 0.
 				loglike[i_step+1] = loglike_new
+				loglike_old = loglike_new + 0.
 				step_stay = False
 
 			# reject new step
 			else: pass
 
-	return hyper, loglike, repeat
+	return hyper_chain, loglike, repeat, i_step
 
 
 

mcmc_general/test_multiline.py
@@ -22,10 +22,10 @@ np.random.seed(seed)
 
 hyper_c1 = 3.
 hyper_c0 = 5.
-n_group = 3
-n_point = 50
-hyper_sigc1 = 1e-1
-hyper_sigc0 = 1e-1
+n_group = 5
+n_point = 10
+hyper_sigc1 = 1e0
+hyper_sigc0 = 1e0
 
 local_c1 = np.random.normal( loc=hyper_c1, scale = hyper_sigc1, size=n_group )
 local_c0 = np.random.normal( loc=hyper_c0, scale = hyper_sigc0, size=n_group )
@@ -33,7 +33,7 @@ local_c0 = np.random.normal( loc=hyper_c0, scale = hyper_sigc0, size=n_group )
 x_real = np.zeros((n_point,n_group))
 y_data = np.zeros((n_point,n_group))
 y_err = np.zeros((n_point,n_group))
-err_size = 3.
+err_size = 2.
 
 for i in range(n_group):
 	x_real[:,i] = np.sort(np.random.uniform(-5,5,n_point))
@@ -41,6 +41,8 @@ for i in range(n_group):
 	y_err[:,i] = err_size * np.random.random(n_point)
 	y_data[:,i] = y_real + np.random.normal(loc=0., scale=y_err[:,i], size=n_point)
 
+print 'hyper: c1 * x + c0', hyper_c1, hyper_c0, hyper_sigc1, hyper_sigc0
+print 'local (c0,c1):', local_c0, local_c1
 
 
 '''
@@ -63,15 +65,14 @@ def draw_local_func(hyper):
 
 ### mcmc
 n_hyper = 4 # hyper_c1, hyper_c0, hyper_sigc1, hyper_sigc0
-hyper0 = np.array([hyper_c1,hyper_c0,hyper_sigc1,hyper_sigc0]) 
-hyper_step = np.array([1e-2,1e-2,1e-2,1e-2]) # randomly selected step size
+hyper0 = 2.*np.array([hyper_c1,hyper_c0,hyper_sigc1,hyper_sigc0]) 
+hyper_step = np.array([3e-1,3e-1,1e-1,1e-1]) # randomly selected step size
 
-hyper_seq, loglike_seq, repeat_seq = \
+hyper_seq, loglike_seq, repeat_seq, i_step = \
 hbm(hyper0, hyper_step, n_step, draw_local_func, n_group, single_log_likely, model, \
 data=[x_real,y_data,y_err], seed=2357, domain=[[2,0,np.inf],[3,0,np.inf]], \
-draw_times=100, single_jump = True, trial_upbound = 10000 )
+draw_times=20, single_jump = False, trial_upbound = 1e6 )
 
-print hyper_seq[0,:]
 
 
 ### plot
@@ -84,25 +85,27 @@ ax = ((a00,a01,a02,a03),(a10,a11,a12,a13))
 #for j in range(col):
 	#ax[0][j].plot(np.repeat(hyper_seq[j,:],repeat),'b-')
 
-
 for i_group in range(n_group):
-	ax[0][0].errorbar(x_real[:,i_group],y_data[:,i_group],yerr = y_err[:,i_group],fmt='r.')
-	ax[0][0].plot(x_real[:,i_group],model(x_real[:,i_group],(local_c0[i], local_c1[i])),'b-')
+	ax[0][0].errorbar(x_real[:,i_group],y_data[:,i_group],yerr = y_err[:,i_group],fmt='.')
+	ax[0][0].plot(x_real[:,i_group],model(x_real[:,i_group],(local_c0[i_group], local_c1[i_group])),'-')
 ax[0][0].legend(['c0 %.1f' %hyper_c0, 'c1 %.1f' %hyper_c1, 'sig_c0 %.1f' %hyper_sigc0, 'sig_c1 %.1f' %hyper_sigc1],\
 loc=0)
 
-ax[0][1].plot(repeat_seq,'b-')
-ax[0][1].set_ylabel('repeat times')
+ax[0][1].plot(repeat_seq[:i_step],'bx-')
+ax[0][1].set_xlabel('repeat times')
 
 delta_log = loglike_seq[1:] - loglike_seq[:-1]
 ratio  = np.exp(delta_log)
 ratio[np.where(ratio>1)[0]] = 1
-ax[0][2].plot(ratio, 'b-')
-ax[0][2].set_ylabel('ratio')
+ax[0][2].plot(ratio[:i_step-1], 'bx-')
+ax[0][2].set_xlabel('ratio')
+
+ax[0][3].plot(loglike_seq[:i_step],'bx-')
+ax[0][3].set_xlabel('loglike')
 
 
 for j in range(col):
-	ax[1][j].plot(hyper_seq[j,:],'b-')
+	ax[1][j].plot(hyper_seq[j,:i_step],'bx-')
 
 
 ax[1][0].set_xlabel('hyper_c1')